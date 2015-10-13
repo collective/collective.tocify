adde8b8614dc42e9996adba41a929fa26fcf8f64
Jon Pentland <jon@iomedia.co.uk>
2015-10-13 17:37:08 +0300

    Add some default options. Parse options from html.

--- src/mockup-tocify/js/patterns/tocify/pattern.js
+++ src/mockup-tocify/js/patterns/tocify/pattern.js
@@ -9,12 +9,18 @@ define([
 ], function (Base, $) {
   'use strict';
 
+
   var Tocify = Base.extend({
     name: 'tocify',
     trigger: '.pat-tocify',  // has to be exact like this: 'pat-' + patternname.
+    parser: 'mockup',
+    defaults: {
+      "selectors": "h2, h3",
+      "showAndHide": true
+    },
     init: function () {
       var self = this;
-      self.$el.tocify();
+      self.$el.tocify(self.options);
     }
   });
   return Tocify;
