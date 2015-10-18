504967b6618a35e8e3e8b4a2aaa080dfa51d855c
Coen van der Kamp <c.van.der.kamp@zestsoftware.nl>
2015-10-18 12:30:39 +0300

    Add tocify pattern options.

--- src/mockup-tocify/js/patterns/tocify/pattern.js
+++ src/mockup-tocify/js/patterns/tocify/pattern.js
@@ -1,7 +1,27 @@
 /* Tocifypattern
 *
 * Options:
-*    selectors(string): The selectors used to build the table of contents.
+*    context(string): Any valid jQuery selector. ("body")
+*    selectors(string): The selectors used to build the table of contents. ("h1,h2,h3")
+*    showAndHide(boolean): Expand and collapse sub headings. (true)
+*    showEffect(string): "none", "fadeIn", "show", or "slideDown", or any of the other jQuery show effects. ("slideDown")
+*    showEffectSpeed(string or integer): "slow", "medium", "fast", or milliseconds. ("medium")
+*    hideEffect(string): "none", "fadeOut", "hide", "slideUp", or any of the jQuery hide effects. ("none")
+*    hideEffectSpeed(string or integer): "slow", "medium", "fast" or milliseconds. ("medium")
+*    smoothScroll(boolean): true or false. (true)
+*    smoothScrollSpeed(string or integer): Accepts Number (milliseconds) or String: "slow", "medium", or "fast". ("medium")
+*    scrollTo(integer or function): Accepts any number of pixels or function. (0)
+*    showAndHideOnScroll(boolean): true or false. (true)
+*    highlightOnScroll(boolean): true or false. (true)
+*    highlightOffset(number): Accepts any number of pixels. (40)
+*    theme(string): The theme "bootstrap", "jqueryui", or "none". ("bootstrap")
+*    extendPage(boolean): true or false. (true)
+*    extendPageOffset(integer): Any number of pixels. (100)
+*    history(boolean): true or false (true)
+*    hashGenerator(string or function): "compact", "pretty", function(text, element){}. Compact - #CompressesEverythingTogether. Pretty - #looks-like-a-nice-url-and-is-easily-readable. function(text, element){} - Your own hash generation function that accepts the text as an argument, and returns the hash value. ("compact")
+*    highlightDefault(boolean): true or false. (true)
+*    ignoreSelector(string): Any valid jQuery selector. (null)
+*    scrollHistory(boolean): true or false. (false)
 *
 * Documentation:
 *   # Tocify
